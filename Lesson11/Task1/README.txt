ac2d176f5fef33d6149fe605ccb8b9544c23a701
Nazarkon <nazarmontsibovych@gmail.com>
2019-05-08 23:43:30 +0300

    Add homework using JS

---
+++ Lesson11/Task1/README.txt
@@ -0,0 +1,1 @@
+In this task was tested some arithmetic operation
---
+++ Lesson11/Task1/js/script.js
@@ -0,0 +1,12 @@
+var a=11 ,b=2, c;
+c = a%b;
+console.log(c);
+
+var a = 36, b = 3, c;
+c = a%b;
+console.log(c);
+
+var a = "5",b = 2, c;
+c = a+b;
+console.log(c);
+
