e3da8b6d4181a9fe62fcaab26a34da1226b565d3
Nazarkon <nazarmontsibovych@gmail.com>
2019-06-08 19:24:04 +0300

    Add details

--- Lesson19/Task7/js/script.js
+++ Lesson19/Task7/js/script.js
@@ -1,33 +1,16 @@
-function limitation(event){
+function limitation(event) {
 
- var x = event.which || event.keyCode;
+    var x  = String.fromCharCode(event.keyCode);
+    if (x < 48) {
+        console.log("Sorry the Unicode number smaller then we need");
+    } else if (x > 57) {
+        console.log("Sorry the Unicode number bigger then we need");
+    } else if (x < 96) {
+        console.log("Sorry the Unicode number smaller then we need");
+    } else if (x > 105) {
+        console.log("Sorry the Unicode number bigger then we need");
+    } else {
+        document.getElementById("demo").innerHTML = "The Unicode value is:" + x;
 
-
-
-
-    switch (x) {
-
-        case x < 48:
-            console.log("Sorry the Unicode number smaller then we need");
-        case x > 57:
-            console.log("Sorry the Unicode number bigger then we need");
-        case x < 96:
-            console.log("Sorry the Unicode number smaller then we need");
-        case x > 105:
-            console.log("Sorry the Unicode number bigger then we need");
-        default:
-            document.getElementById("demo").innerHTML = "The Unicode value is:" + x;
     }
-//  if(event.which < 48){
-//     console.log("Sorry the Unicode number smaller then we need");
-//  }else if(event.which > 57){
-//     console.log("Sorry the Unicode number bigger then we need");
-//  }else if(event.which < 96){
-//     console.log("Sorry the Unicode number smaller then we need");
-//  }else if(event.which > 105){
-//     console.log("Sorry the Unicode number bigger then we need");
-//  }else{
-//    document.getElementById("demo").innerHTML = "The Unicode value is:" + x;
- 
-// }
 }
