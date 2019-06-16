276036ec13491f32ee90d61eb362a82a5b1c2254
Nazarkon <nazarmontsibovych@gmail.com>
2019-06-16 17:49:43 +0300

    Update my jquery

--- Lesson20/Task1/myjscode/script_index.js
+++ Lesson20/Task1/myjscode/script_index.js
@@ -1,15 +1,61 @@
-$("header").each(function(index){
-    console.log(index + ":" + $(this).html());
-});
-$("#nav").each(function(index){
-    console.log(index + ":" + $(this).html());
-});
-
-
+console.log($("body").children());
+console.log($("#wrapper").children());
 console.log($("#header").children());
-console.log($("#nav").children().html());
+$("header").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+console.log($("#nav").children());
+$("#nav").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
 console.log($("#main").children());
+
+
 console.log($("#intro").children());
-console.log($(".spotlight").children().nextUntil("#first"));
-console.log($("body.is-preload").children());
-console.log($("#wrapper").children(),$("#header").find());
+$("#intro").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+console.log($(".spotlight").children());
+$(".spotlight").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+console.log($("#first").children());
+$("#first").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+console.log($(".header").children());
+$(".header").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+
+console.log($("#second").children());
+$("#second").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+console.log($(".major").children());
+$(".major").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+console.log($("#cta").children());
+$("#cta").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+console.log($(".major").children());
+$(".major").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+console.log($("#footer").children());
+$("#footer").each(function (index) {
+    console.log(index + ":" + $(this).children().html());
+});
+
+
+
